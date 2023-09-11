ff12d8582b4d3efad3c6d308ba988256d3c29ebf
Caleb Tomkins <cptomkins@gmail.com>
2023-09-10 21:29:15 -0400

    just throw these things up there

--- include/screen.hpp
+++ include/screen.hpp
@@ -9,11 +9,11 @@ class Screen {
 public:
     Screen::Screen(sf::RenderWindow& window);
 
-    virtual void handleEvent(sf::Event event) = 0;
-    virtual void update() = 0;
-    virtual void render(sf::RenderWindow& window) = 0;
+    void handleEvent(sf::Event event);
+    void update();
+    void render(sf::RenderWindow& window);
     // Add other common functionality or data members here if needed
-    virtual void createBackground(sf::RenderWindow& window);
+    void createBackground(sf::RenderWindow& window);
 
 private:
     sf::Color backgroundColor; // Store the background color as a member variable
